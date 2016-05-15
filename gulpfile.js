1577cdabfe2ecaeb4d3bd334c9286cefebd43cfe
howdy <zxzhang5@mail.ustc.edu.cn>
2016-05-15 21:38:11 +0800

    变量修改

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,9 @@ gulp.task('sass', function () {
             .pipe(autoprefixer({browsers: ['last 2 version', 'ie 8', 'ie 9', 'ios 6', 'android 4']}))
             .pipe(sourcemaps.write('./maps'))
             .pipe(gulp.dest('./dist/css'))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(cleanCSS())
-            .pipe(gulp.dest('./dist/css'));
+//            .pipe(rename({suffix: '.min'}))
+//            .pipe(cleanCSS())
+//            .pipe(gulp.dest('./dist/css'));
 });
 gulp.task('install', function () {
     gulp.src('./node_modules/font-awesome/fonts/*.{otf,ttf,woff,woff2,eot,svg}')
