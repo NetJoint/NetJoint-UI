66880a622b44e12d2ccbc42e7521bc4ecd6f045e
cuanhanshansi <liang_xue_feng@sina.com>
2016-06-20 09:26:10 +0800

    提交修改内容

--- examples/js/forgetPassword.js
+++ examples/js/forgetPassword.js
@@ -13,3 +13,33 @@ oName.onfocus = function(){
 }
 
 
+require.config({
+    paths: {
+        "netjoint-ui": "../../dist/js/netjoint-ui",
+    }
+});
+require(['netjoint-ui'], function () {
+    (function (window, undefined) {
+        'use strict';
+
+        $(function () {
+
+           var oAccount = document.getElementById('accountName');
+           var oName = document.getElementById('name');
+
+           oName.onblur = function(){
+                if(oName.value==''){
+                   oAccount.style.display='inline-block';
+                }else{
+                   oAccount.style.display='none'; 
+                }
+            }
+            oName.onfocus = function(){
+                oAccount.style.display='none';
+             }
+
+        });
+    })(window);
+});
+
+
--- examples/js/register.js
+++ examples/js/register.js
@@ -1,20 +1,33 @@
-var oSelectClause = document.getElementById('selectClause');
-var oAgreeClause = document.getElementById('agreeClause');
-var oRegister = document.getElementById('register');
+require.config({
+    paths: {
+        "netjoint-ui": "../../dist/js/netjoint-ui",
+    }
+});
+require(['netjoint-ui'], function () {
+    (function (window, undefined) {
+        'use strict';
+
+        $(function () {
+
+            var oSelectClause = document.getElementById('selectClause');
+            var oAgreeClause = document.getElementById('agreeClause');
+            var oRegister = document.getElementById('register');
 
 //判断复选框是否选中
-oSelectClause.onclick = function(){
-    if(oSelectClause.checked==false){
-        oAgreeClause.style.display = 'block';
-    }
-    else{
-        oAgreeClause.style.display = 'none';
-    }
-}
+            oSelectClause.onclick = function () {
+                if (oSelectClause.checked == false) {
+                    oAgreeClause.style.display = 'block';
+                } else {
+                    oAgreeClause.style.display = 'none';
+                }
+            }
 //单击“立即注册”按钮时判断复选框是否选中
-oRegister.onclick = function(){
-    if(oSelectClause.checked==false){
-        oAgreeClause.style.display = 'block';
-    }
-}
+            oRegister.onclick = function () {
+                if (oSelectClause.checked == false) {
+                    oAgreeClause.style.display = 'block';
+                }
+            }
 
+        });
+    })(window);
+});
