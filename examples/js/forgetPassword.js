03a284cf0e5fdcffbdab325a04fd9d971bd539e1
cuanhanshansi <liang_xue_feng@sina.com>
2016-06-21 09:36:44 +0800

    example

--- examples/js/forgetPassword.js
+++ examples/js/forgetPassword.js
@@ -1,18 +1,3 @@
-var oAccount = document.getElementById('accountName');
-var oName = document.getElementById('name');
-
-oName.onblur = function(){
-    if(oName.value==''){
-        oAccount.style.display='inline-block';
-    }else{
-        oAccount.style.display='none'; 
-    }
-}
-oName.onfocus = function(){
-    oAccount.style.display='none';
-}
-
-
 require.config({
     paths: {
         "netjoint-ui": "../../dist/js/netjoint-ui",
